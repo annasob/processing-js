97eee860a257b562133838d056c064a8ca7b593d
Anna Sobiepanek <annaopensource at gmail.com>
2011-05-24 16:51:59 -0400

    updated init function [#1261 state:peer-review-requested assigned:Pomax]

--- processing.js
+++ processing.js
@@ -19477,60 +19477,22 @@
 
     for (var i = 0, l = canvas.length; i < l; i++) {
       // datasrc and data-src are deprecated.
-      var processingSources = canvasElements[i].getAttribute('data-processing-sources');
+      var processingSources = canvas[i].getAttribute('data-processing-sources');
       if (processingSources === null) {
         // Temporary fallback for datasrc and data-src
-        processingSources = canvasElements[i].getAttribute('data-src');
+        processingSources = canvas[i].getAttribute('data-src');
         if (processingSources === null) {
-          processingSources = canvasElements[i].getAttribute('datasrc');
+          processingSources = canvas[i].getAttribute('datasrc');
         }
       }
       if (processingSources) {
-        filenames = processingSources.split(' ');
+        var filenames = processingSources.split(' ');
         for (var j = 0; j < filenames.length;) {
           if (filenames[j]) {
             j++;
           } else {
             filenames.splice(j, 1);
           }
-        }
-        loadAndExecute(canvasElements[i], filenames);
-      }
-    }
-    
-    // process all <script>-indicated sketches
-    var scripts = document.getElementsByTagName('script');
-    var s, source, instance;
-    for (s = 0; s < scripts.length; s++) {
-      var script = scripts[s];
-      if (!script.getAttribute) {
-        continue;
-      }
-
-      var type = script.getAttribute("type");
-      if (type && (type.toLowerCase() === "text/processing" || type.toLowerCase() === "application/processing")) {
-        var target = script.getAttribute("data-target");
-        var canvas;
-        if (target) {
-          canvas = document.getElementById(target);
-        } else {
-          var nextSibling = script.nextSibling;
-          while (nextSibling && nextSibling.nodeType !== 1) {
-            nextSibling = nextSibling.nextSibling;
-          }
-          if (nextSibling.nodeName.toLowerCase() === "canvas") {
-            canvas = nextSibling;
-          }
-        }
-
-        if (canvas && canvas !== null) {
-          if (script.getAttribute("src")) {
-            filenames = script.getAttribute("src").split(/\s+/);
-            loadAndExecute(canvas, filenames);
-            continue;
-          }
-          source =  script.innerText || script.textContent;
-          instance = new Processing(canvas, source);
         }
         loadSketchFromSources(canvas[i], filenames);
       }
